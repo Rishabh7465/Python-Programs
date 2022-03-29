d044cd818a803107eb1775633756e3ad54347fbe
Rishabh Sharma <76515114+Rishabh7465@users.noreply.github.com>
2022-03-29 17:48:54 +0530

    Delete LowerNumber.py

--- LowerNumber.py
+++
@@ -1,7 +0,0 @@
-#Q4. Write a program to find the lowest number out of two numbers excepted from user.
-num1 = int(input("Enter 1st number"))
-num2 = int(input("Enter 2nd Number"))
-if(num1<num2):
-    print("1st is lower", num1)
-else:
-    print("2nd is lower", num2)
