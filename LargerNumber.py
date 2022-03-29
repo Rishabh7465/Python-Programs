369c4847ac411555cb431868b44ededdec5f604a
Rishabh Sharma <76515114+Rishabh7465@users.noreply.github.com>
2022-03-29 17:24:20 +0530

    Add files via upload

---
+++ LargerNumber.py
@@ -0,0 +1,8 @@
+#Q5. Write a program to find the largest number out of two numbers excepted from user.
+#Q4. Write a program to find the lowest number out of two numbers excepted from user.
+num1 = int(input("Enter 1st number"))
+num2 = int(input("Enter 2nd Number"))
+if(num1>num2):
+    print("1st is Larger", num1)
+else:
+    print("2nd is Larger", num2)
---
+++ SeniorCitizen.py
@@ -0,0 +1,6 @@
+#Q3. Write a program to check whether a person is senior citizen or not.
+age = int(input("Enter your Age: "))
+if(age>=60):
+    print("Senior Citizen ")
+else:
+    print("Not Senior Citizen")
