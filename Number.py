0e086ab946b62e5a57d1f8cedfe4adaedb92eb1b
Rishabh Sharma <76515114+Rishabh7465@users.noreply.github.com>
2022-03-29 17:49:12 +0530

    Delete Number.py

--- Number.py
+++
@@ -1,6 +0,0 @@
-#Q6. Write a program to check whether a number (accepted from user) is positive or negative.
-num = int(input("Enter the number"))
-if(num<0):
-    print("negative number")
-else:
-    print("Positive number")
